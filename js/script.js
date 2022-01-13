0c0ea621456143985485e7b5dfd187cef587eec1
Fernando Mallet <fernandomallet@outlook.com>
2022-01-13 13:36:34 -0300

    proyecto

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 ///// Variables
 
 let listaProductos = [];
+let respuesta = 0;
 
 ///// Clases
 
@@ -20,7 +21,8 @@ class Producto {
 
 function crearProducto() {
     let nombre = prompt("Ingrese el nombre del nuevo producto:");
-    while (comprobarExistencia(nombre)) {
+    while (buscarProducto(nombre) !== -1) {
+        alert("El producto ingresado ya existe");
         nombre = prompt("Ingrese el nombre del nuevo producto:");
     }
     let precio = parseFloat(prompt("Ingrese el precio del nuevo producto:"));
@@ -28,14 +30,10 @@ function crearProducto() {
     return new Producto(nombre, precio, cantidad);
 }
 
-function comprobarExistencia(nombre) {
-    if (listaProductos.find((producto) => producto.nombre === nombre)) {
-        alert("El producto ingresado ya existe");
-        return true;
-    }
-    else {
-        return false;
-    }
+// Devuelve -1 si no existe el producto. Si existe devuelve el índice
+function buscarProducto(nombre) {
+    let productoEnLista = listaProductos.find((producto) => producto.nombre === nombre);
+    return listaProductos.indexOf(productoEnLista);
 }
 
 function agregarProducto() {
@@ -44,8 +42,16 @@ function agregarProducto() {
     console.log(`Nuevo producto agregado. Son ${nuevoProducto.cantidad} productos de tipo ${nuevoProducto.nombre} a $${nuevoProducto.precio} cada uno. Representan un valor de $${nuevoProducto.calcularValor()}`);
 }
 
+function eliminarProducto(nombre) {
+    let indiceProducto = buscarProducto(nombre);
+    if(indiceProducto !== -1)
+    {
+        listaProductos.splice(indiceProducto, 1)
+    }
+}
+
 function ordenarProductos(criterio) {
-    if (criterio === "ascendente")
+    if (criterio === "ascendente") {
         listaProductos.sort((a, b) => {
             if (a.nombre < b.nombre) {
                 return -1;
@@ -56,6 +62,7 @@ function ordenarProductos(criterio) {
                 return 0;
             }
         });
+    }
     else if (criterio === "descendente") {
         listaProductos.sort((a, b) => {
             if (a.nombre > b.nombre) {
@@ -71,7 +78,6 @@ function ordenarProductos(criterio) {
     else {
         console.error("Error: criterio de ordenación inválido");
     }
-    console.log(listaProductos);
 }
 
 function calcularValorTotal() {
@@ -83,3 +89,45 @@ function calcularValorTotal() {
 
     console.log(`El valor de todos los productos en stock es de $${valorTotal}`);
 }
+
+function mostrarProductos() {
+    console.log(listaProductos);
+}
+
+///// Main
+
+alert("Se pedirá que ingrese los datos de 4 tipos de productos distintos para agregar al stock.")
+
+agregarProducto();
+agregarProducto();
+agregarProducto();
+agregarProducto();
+calcularValorTotal();
+
+// Ordenamiento de la lista
+respuesta = prompt("Ingrese el número correspondiente al criterio que desea utilizar para ordenar la lista de productos:\n1. Ascendente\n2. Descendente\n");
+
+while ((respuesta !== "1") && (respuesta !== "2") && (respuesta === null)) {
+    alert("La opción ingresada no es válida.")
+    respuesta = prompt("Ingrese el número correspondiente al criterio que desea utilizar para ordenar la lista de productos:\n1. Ascendente\n2. Descendente\n");
+}
+
+if(respuesta === "1") {
+    ordenarProductos("ascendente");
+} else {
+    ordenarProductos("descendente");
+}
+
+mostrarProductos();
+
+// Eliminación del producto
+respuesta = prompt("Ingrese el nombre del producto que desea eliminar:");
+
+while (buscarProducto(respuesta) === -1) {
+    alert("El producto ingresado no existe.")
+    respuesta = prompt("Ingrese el nombre del producto que desea eliminar:");
+}
+
+eliminarProducto(respuesta);
+alert("El producto " + respuesta + " fue eliminado.")
+mostrarProductos();
