e84d5bd815a300ce106f9ac60d71be313ea25673
Fernando Mallet <fernandomallet@outlook.com>
2026-01-06 10:11:10 -0300

    corrección de rutas para despliegue en GitHub Pages

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 const productsTable = "productsTable";
 const newProductForm = "newProductForm";
 const themesContainer = "themesContainer";
-const productsURL = "./Administrador-de-Stock/json/products.json";
+const productsURL = "./json/products.json";
 const themes = [{ name: "defaultTheme", color: "#212529" }, { name: "blueTheme", color: "#0d6efd" }, { name: "redTheme", color: "#dc3545" }, { name: "greenTheme", color: "#198754" }];
 let theme;
 let products;
