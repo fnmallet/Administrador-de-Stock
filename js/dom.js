0643f0c9da71e484fe472410ef9eb1914ae842cb
Fernando Mallet <fernandomallet@outlook.com>
2026-01-06 10:15:34 -0300

    corrección de rutas para despliegue en GitHub Pages

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,5 @@
 function createTableRows(tableId, products) {
-    for(const product of products) {
+    for (const product of products) {
         $("#" + tableId + " tbody").append(`<tr>
                                 <td class="nameCell"><input class="editableInputCell" value="${product.name}" readonly data-value="" data-property="name"></input></td>
                                 <td class="categoryCell editableCell"><input class="editableInputCell" value="${product.category}" readonly data-value="" data-property="category"></input></td>
@@ -7,7 +7,7 @@ function createTableRows(tableId, products) {
                                 <td class="amountCell editableCell text-center"><input class="editableInputCell text-center" value="${product.amount}" readonly data-value="" data-property="amount"></input></td>
                                 <td class="deleteCell">
                                     <button class="btn" type="button" onclick="deleteProduct(products, this, '${product.name}')">
-                                        <img class="deleteButtonImg" src="/assets/images/icons/trash.svg" alt="Eliminar producto.">
+                                        <img class="deleteButtonImg" src="./assets/images/icons/trash.svg" alt="Eliminar producto.">
                                     </button>
                                 </td>
                             </tr>`);
@@ -15,11 +15,11 @@ function createTableRows(tableId, products) {
 }
 
 function displayElement(id) {
-    $("#"+id).removeClass("d-none");
+    $("#" + id).removeClass("d-none");
 }
 
 function hideElement(id) {
-    $("#"+id).addClass("d-none");
+    $("#" + id).addClass("d-none");
 }
 
 function deleteProduct(products, button, productName) {
@@ -29,7 +29,7 @@ function deleteProduct(products, button, productName) {
 }
 
 function createThemeButtons(themes, themesContainer) {
-    for(const theme of themes) {
+    for (const theme of themes) {
         $("#" + themesContainer).append(`<button class="btn">
                                             <svg>
                                                 <circle style="fill: ${theme.color}" cx="20" cy="20" r="20"/>
